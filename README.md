ad249ee09a419e06b2eb3cce6a11955c0059b2e7
Renato Junior <renatojr.dev@gmail.com>
2023-01-06 13:38:38 -0300

    piano finished

---
+++ README.md
@@ -0,0 +1,5 @@
+# Digital Piano
+
+Digital piano developed with HTML, CSS and JavaScript.
+
+<li><a href="https://youtu.be/Mv1ZYpo8q4g" target="_blank">Learn how to do it with this video</a></li>
--- main.js
+++ main.js
@@ -1,16 +1,42 @@
-const keyboardKeys = document.querySelectorAll('.key')
+const pianoKeys = document.querySelectorAll('.piano-keys .key');
+const volumeSlider = document.querySelector('.volume-slider input');
+const keysCheckbox = document.querySelector('.keys-checkbox input');
+let allKeys = [];
+let audio = new Audio('audio/w.wav');
 
-for (let i = 0; i < keyboardKeys.length; i++) {
-    const keyboardKey = keyboardKeys[i]
-    const keySound = `#${keyboardKey.id}_sound` 
+function playTune (key) {
+    audio.src = `audio/${key}.wav`;
+    audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
     
-    keyboardKey.onclick = function () {
-        playKeySound(keySound)
-    }   
+    clickedKey.classList.add('active');
+    
+    setTimeout(() => {
+        clickedKey.classList.remove('active');
+    }, 150);
 }
 
-function playKeySound (key) {
-    const keySound = document.querySelector(key)
-    keySound.currentTime = 0
-    keySound.play()
+pianoKeys.forEach(key => {
+    allKeys.push(key.dataset.key);
+
+    key.addEventListener('click', () => playTune(key.dataset.key));
+});
+
+function handleVolume (e) {
+    audio.volume = e.target.value;
 }
+
+function showHideKeys () {
+    pianoKeys.forEach(key => key.classList.toggle('hide'));
+}
+
+function pressedKey (e) {
+    if (allKeys.includes(e.key)) {
+        playTune(e.key);
+    }
+}
+
+volumeSlider.addEventListener('input', handleVolume);
+keysCheckbox.addEventListener('click', showHideKeys);
+document.addEventListener('keydown', pressedKey);
